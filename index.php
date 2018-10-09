9c7ea621cc16b00cd0da816c67712ad904ce445f
youpuppey <youpuppey@github.com>
2018-10-09 21:56:48 +0800

    test

--- index.php
+++ index.php
@@ -127,7 +127,7 @@
   		<div class="media">
 		  <div class="media-body">
 		    <h5 class="mt-0 mb-1">Human Resource Management</h5>
-		    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+		    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus
 		  </div>
 		  <img class="ml-3" src="img/hricon.png" width="110px">
 		</div>
